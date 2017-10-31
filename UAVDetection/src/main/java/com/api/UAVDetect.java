6623b670746dea1fb837d978cd6758dab9a7d6eb
iec-hust <iec.hust.edu@gmail.com>
2017-10-31 09:00:48 +0800

    UAVdetect

--- UAVDetection/src/main/java/com/api/UAVDetect.java
+++ UAVDetection/src/main/java/com/api/UAVDetect.java
@@ -1,9 +1,10 @@
 package com.api;
 
-//import com.google.gson.Gson;
 import com.model.ImageInfo;
 import com.util.UAVHelper;
 import com.util.helper.FileLogger;
+
+import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
 /**
@@ -21,32 +22,56 @@ public class UAVDetect {
     }
 
     public static void main(String[] args) throws Exception{
+        if (args.length < 1){
+            System.out.println("use : args[0] -- iamge txt file path .");
+            return;
+        }
         UAVDetect uavDetect = new UAVDetect();
-        uavDetect.hello();
+        //uavDetect.hello();
         uavDetect.init("/home/hadoop/zm/ssd/model/deploy.prototxt", "/home/hadoop/zm/ssd/model/VGG_VOC0712_SSD_300x300_iter_120000.caffemodel");
-        //ImageInfo imageInfo = new ImageInfo("/home/hadoop/zm/uav-data/UAV-Images-raw/00168.jpg");
+
+        ImageInfo imageInfo;
         long start = System.currentTimeMillis();
-        List<float[]> list = null;
-        for (int i = 0; i < 50; i++){
-            ImageInfo imageInfo = new ImageInfo("/home/hadoop/zm/test-image/2.jpg");
-            float[][] result = uavDetect.detect(imageInfo);
-            fileLogger.log(" time deley :" , (System.currentTimeMillis() - start) + "");
-            start = System.currentTimeMillis();
-            float[] realResult = UAVHelper.filterByScore(result);
-            for (int j = 0; j < realResult.length; j++) {
-                System.out.print(realResult[j] +  ",  ");
-            }
-            System.out.println();
+        File file=new File(args[0]);
+        BufferedReader reader=null;
+        String temp = null;
+        reader=new BufferedReader(new FileReader(file));
+        FileWriter fw = new FileWriter(new File("/home/hadoop/zm/cor.txt"));
+        BufferedWriter bw = new BufferedWriter(fw);
+            while((temp=reader.readLine())!=null){
+                System.out.println("image = " + temp );
+                imageInfo = new ImageInfo(temp);
+                //ImageInfo imageInfo = new ImageInfo("/home/hadoop/zm/ssd/images/1/1_0361.jpg");
+                float[][] result = uavDetect.detect(imageInfo);
+                fileLogger.log(" time deley = " , (System.currentTimeMillis() - start) + "");
+                start = System.currentTimeMillis();
+
+                float[] realResult = UAVHelper.filterByScore(result);
+                for (int j = 0; j < realResult.length; j++) {
+                    System.out.print((int)realResult[j] +  ",  ");
+                }
+                System.out.println();
+                bw.write(temp + " " + (int)realResult[3] + " " + (int)realResult[4] + " " + (int)realResult[5] + " " + (int)realResult[6] + "\n");
+                //Thread.sleep(3000);
 
         }
+        bw.close();
+        fw.close();
+        reader.close();
 
-        //Gson gson = new Gson();
-        //System.out.println(gson.toJson(result));
-//        for (int i = 0; i < result.length; i++){
-//            for (int j = 0; j < result[i].length; j++)
-//                System.out.print(result[i][j] + "  ");
-//            System.out.println();
-//        }
+
+
+
+        //if mem out
+
+/*
+        int count = 0 ;
+        while (count++ < 100) {
+            ImageInfo imageInfo = new ImageInfo("/home/hadoop/zm/ssd/images/1/1_0564.jpg");
+            uavDetect.detect(imageInfo);
+            System.out.println("detect : " +  count);
+        }
+*/
 
     }
 }
